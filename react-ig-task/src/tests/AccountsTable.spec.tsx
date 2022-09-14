60028e29eb02ece70f0991240f9b6764d59d3784
Michał Woźniak <lachimek1@gmail.com>
2022-09-14 23:13:48 +0200

    imports cleanup

--- react-ig-task/src/tests/AccountsTable.spec.tsx
+++ react-ig-task/src/tests/AccountsTable.spec.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { AccountsTable } from "../components/AccountsTable";
